19b75f3efbf6ec4ecaef7b5b2b173f61c4472377
prophesiercc <bwknowles98@gmail.com>
2024-03-13 18:30:27 -0400

    update

---
+++ work/2_23/index.js
@@ -0,0 +1,26 @@
+const button1 = () =>
+{
+  const newElement = document.getElementById('first-list');
+  const p = document.createElement('p');
+  p.innerText = 'bio';
+  newElement.appendChild(p);
+  console.log('test1');
+}
+
+const button2 = () =>
+{
+  console.log('test2');
+
+}
+
+const button3 = () =>
+{
+  console.log('test3');
+
+}
+
+const button4 = () =>
+{
+  console.log('test4');
+
+}

8c549efb87c0b0ac8a45104af4f9820e5db42293
ProphesierC <102403526+prophesierc@users.noreply.github.com>
2024-07-17 01:50:30 -0400

    modified memoization.js

--- work/Exercism_Practise/memoization.js
+++ work/Exercism_Practise/memoization.js
@@ -3,31 +3,32 @@
  * @return {Function}
  */
 const memoize = (fn) => 
-{
-    const cache = {};
-
-    return function(...args) 
     {
-        const key = args.join(',');
-
-        if (key in cache) 
+        const cache = {};
+    
+        return function(...args)
         {
-            return cache[key];
+            const key = args.join(',');
+            
+            if (key in cache) 
+            {
+                return cache[key];
+            }
+    
+            const result = fn(...args);
+            cache[key] = result;
+            return result;
         }
-
-        const result = fn(...args);
-        cache[key] = result;
-        return result;
     }
-}
     
     
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) 
-{
-    callCount += 1;
-    return a + b;
-})
-memoizedFn(2, 3) // 5
-memoizedFn(2, 3) // 5
-console.log(callCount) // 1 
+
+ let callCount = 0;
+ const memoizedFn = memoize(function (a, b) {
+	 callCount += 1;
+   return a + b;
+ })
+ 
+ console.log(callCount) // 1 
+ console.log(memoizedFn(2,3)) // 5
+ 

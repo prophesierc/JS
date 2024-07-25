0352a6c26969cbd5ed018278accc1562e45f4b05
ProphesierC <bwknowles98@gmail.com>
2024-07-25 00:56:47 -0400

    pyramidMaker.js

---
+++ work/Exercism_Practise/pyramidMaker.js
@@ -0,0 +1,19 @@
+const rightTriangle = (h) =>
+{
+    for (let x = 0; x < h; x++) 
+    {
+        console.log(('#'.repeat(x+1)))
+    }
+}
+
+const rt = rightTriangle(10);
+rt
+
+const equaliateralTriangle = (h) =>
+{
+    for (let x = 0; x < h; x++) 
+    {
+        console.log(('#'.repeat(x+1)))
+    }
+}
+// console.log(equaliateralTriangle(10));
